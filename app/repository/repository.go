05acaf072201b4be8e4ab680a275b41340e96492
Zaid Hwang <gwhwang@olimplanet.com>
2025-12-05 18:28:00 +0900

    feat: gorm transaction WithContext 추가

--- app/repository/repository.go
+++ app/repository/repository.go
@@ -30,6 +30,13 @@ func (r *repository) WithTransaction(ctx context.Context, fn func(txRepo domain.
 		return tx.Error
 	}
 
+	defer func() {
+		if rc := recover(); rc != nil {
+			tx.Rollback()
+			panic(rc)
+		}
+	}()
+
 	txRepo := &repository{
 		externalGormClient: r.externalGormClient.NewTxDB(tx),
 	}
